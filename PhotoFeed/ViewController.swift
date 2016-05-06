7610958b437d24ddbc5469c77195572c199d7c81
Myoung-Wan Koo <mwkoo9@gmail.com>
2016-05-06 12:39:33 +0900

    Starting

--- PhotoFeed/ViewController.swift
+++ PhotoFeed/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    @IBOutlet weak var updateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,7 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func updateButtonAction(sender: AnyObject) {
+    }
 }
 
